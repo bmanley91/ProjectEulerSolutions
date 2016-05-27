d23530cd03d99c0091f078406cde46ba418577eb
bmanley <bmanley@commercehub.com>
2016-05-27 15:49:26 -0400

    Added solution for problem 1.

---
+++ src/main/Problem1/MultiplesOf3Or5.java
@@ -0,0 +1,48 @@
+package Problem1;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Get the sum of all multiples of 3 or 5 under 1000.
+ */
+public class MultiplesOf3Or5 {
+
+    private static final int MAX_VALUE = 1000;
+
+    public static void main(String args[]) {
+        List<Integer> multiples = findMultiplesUnderMax(MAX_VALUE);
+
+        System.out.println(sumList(multiples));
+    }
+
+    static List<Integer> findMultiplesUnderMax(int maxValue) {
+        List<Integer> returnList = new ArrayList<Integer>();
+
+        for (int i = 1; i < maxValue; i++) {
+            if (isMultipleOf3(i) || isMultipleOf5(i)) {
+                returnList.add(i);
+            }
+        }
+
+        return returnList;
+    }
+
+    static boolean isMultipleOf3(int value) {
+        return (value % 3) == 0;
+    }
+
+    static boolean isMultipleOf5(int value) {
+        return (value % 5) == 0;
+    }
+
+    static int sumList(List<Integer> integerList) {
+        int returnVal = 0;
+
+        for(int currentInt : integerList) {
+            returnVal += currentInt;
+        }
+
+        return returnVal;
+    }
+}
---
+++ src/test/Problem1/MultiplesOf3Or5Test.java
@@ -0,0 +1,73 @@
+package Problem1;
+
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+/**
+ * Created by bmanley on 5/27/16.
+ */
+@SuppressWarnings("Duplicates")
+public class MultiplesOf3Or5Test {
+
+    @Test
+    public void testFindMultiplesUnderMax() {
+        final int maxValue = 10;
+        final List<Integer> expectedResults = new ArrayList<Integer>() {{
+            add(3);
+            add(5);
+            add(6);
+            add(9);
+        }};
+
+        List<Integer> actualResults = MultiplesOf3Or5.findMultiplesUnderMax(maxValue);
+
+        assertEquals("Expected list not returned.", expectedResults, actualResults);
+    }
+
+    @Test
+    public void testIsMultipleOf3() {
+        final int multipleOf3 = 6;
+
+        assertTrue("Multiple of 3 returned false.", MultiplesOf3Or5.isMultipleOf3(multipleOf3));
+    }
+
+    @Test
+    public void testIsNotMultipleOf3() {
+        final int notMultipleOf3 = 7;
+
+        assertFalse("Non-multiple of 3 returned true.", MultiplesOf3Or5.isMultipleOf3(notMultipleOf3));
+    }
+
+    @Test
+    public void testIsMultipleOf5() {
+        final int multipleOf5 = 10;
+
+        assertTrue("Multiple of 5 returned false.", MultiplesOf3Or5.isMultipleOf5(multipleOf5));
+    }
+
+    @Test
+    public void testIsNotMultipleOf5() {
+        final int notMultipleOf5 = 11;
+
+        assertFalse("Non-multiple of 5 returned true.", MultiplesOf3Or5.isMultipleOf5(notMultipleOf5));
+    }
+
+    @Test
+    public void testSumList() {
+        final List<Integer> testIntegerList = new ArrayList<Integer>() {{
+            add(3);
+            add(5);
+            add(6);
+            add(9);
+        }};
+        final int expectedTotal = 23;
+
+        assertEquals("Sum total did not match expected amount.", expectedTotal, MultiplesOf3Or5.sumList(testIntegerList));
+    }
+}
