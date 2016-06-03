22ecbd20625c5fe22354690eb64c82fa6391dea1
bmanley <bmanley@commercehub.com>
2016-06-03 15:57:29 -0400

    Finished solution for problem 2.

--- src/main/java/Problem2/EvenFibonacciSum.java
+++ src/main/java/Problem2/EvenFibonacciSum.java
@@ -11,11 +11,59 @@ public class EvenFibonacciSum {
 
     public static void main(String args[]) {
 
+        List<Integer> fibonacciNumbers = getFibonacciNumbersUnderMax(MAX_VALUE, null);
+
+        List<Integer> evenFibonacciNumbers = getEvenNumbers(fibonacciNumbers);
+
+        int sum = sumNumbers(evenFibonacciNumbers);
+
+        System.out.println(sum);
     }
 
-    public static List<Integer> getFibonacciNumbersUnderMax(int maxValue) {
-        List<Integer> fibonacciNumbers = new ArrayList<Integer>();
+    static List<Integer> getFibonacciNumbersUnderMax(int maxValue, List<Integer> currentList) {
+        if (currentList == null) {
+            currentList = new ArrayList<Integer>();
+            currentList.add(1);
+            currentList.add(1);
+        }
 
-        return fibonacciNumbers;
+        final int lastNumber = currentList.size() - 1;
+        final int secondToLasNumber = currentList.size() - 2;
+
+        int value1 = currentList.get(lastNumber);
+        int value2 = currentList.get(secondToLasNumber);
+
+        int currentNumber = value1 + value2;
+
+        if (currentNumber >= maxValue) {
+            return currentList;
+
+        } else {
+            currentList.add(currentNumber);
+
+            return getFibonacciNumbersUnderMax(maxValue, currentList);
+        }
+    }
+
+    static List<Integer> getEvenNumbers(List<Integer> input) {
+        List<Integer> returnValue = new ArrayList<Integer>();
+
+        for (int currentNumber : input) {
+            if (currentNumber % 2 == 0) {
+                returnValue.add(currentNumber);
+            }
+        }
+
+        return returnValue;
+    }
+
+    static int sumNumbers(List<Integer> inputList) {
+        int result = 0;
+
+        for (int currentNumber : inputList) {
+            result += currentNumber;
+        }
+
+        return result;
     }
 }
--- src/test/java/Problem2/EvenFibonacciSumTest.java
+++ src/test/java/Problem2/EvenFibonacciSumTest.java
@@ -11,17 +11,43 @@ import static org.junit.Assert.assertEquals;
  * Created by bmanley on 5/27/16.
  */
 public class EvenFibonacciSumTest {
-    private static final List<Integer> expectedResults = new ArrayList<Integer>() {{
+
+    private static final List<Integer> INPUT_LIST = new ArrayList<Integer>() {{
         add(1);
         add(2);
         add(3);
-        add(5);}};
+        add(4);}};
 
     @Test
     public void testGetFibonacciNumbersUnderMax() {
         final int maxValue = 8;
 
+        final List<Integer> expectedResults = new ArrayList<Integer>();
+        expectedResults.add(1);
+        expectedResults.add(1);
+        expectedResults.add(2);
+        expectedResults.add(3);
+        expectedResults.add(5);
+
+
         assertEquals("Results did not match expected list.", expectedResults,
-                EvenFibonacciSum.getFibonacciNumbersUnderMax(maxValue));
+                EvenFibonacciSum.getFibonacciNumbersUnderMax(maxValue, null));
+    }
+
+    @Test
+    public void testGetEvenNumbers() {
+        final List<Integer> expectedResults = new ArrayList<Integer>();
+        expectedResults.add(2);
+        expectedResults.add(4);
+
+        assertEquals("Results did not match expected list.", expectedResults,
+                EvenFibonacciSum.getEvenNumbers(INPUT_LIST));
+    }
+
+    @Test
+    public void testSumNumbers() {
+        final int expectedResult = 10;
+
+        assertEquals("Result did not match expected value.", expectedResult, EvenFibonacciSum.sumNumbers(INPUT_LIST));
     }
 }
