3694f66ea4b86d445e6a7e5dc23a444ba77259c2
bmanley <bmanley@commercehub.com>
2016-06-21 10:45:35 -0400

    Added test for LargestPalindromeProduct.

--- src/test/java/Problem4/LargestPalindromeProductTest.java
+++ src/test/java/Problem4/LargestPalindromeProductTest.java
@@ -3,15 +3,25 @@ package Problem4;
 
 import org.junit.Test;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
 public class LargestPalindromeProductTest {
 
     @Test
+    public void testCheckProductsAboveValue() {
+        final int expectedResult = 906609;
+        final int maxThreeDigitNumber = 999;
+
+        assertEquals("Actual result did not match expected value.", expectedResult,
+                LargestPalindromeProduct.checkProductsAboveValue(maxThreeDigitNumber));
+    }
+
+    @Test
     public void testIsPalindrome() {
-        final String palindrome = "10001";
-        final String notPalindrome = "12345";
+        final int palindrome = 10001;
+        final int notPalindrome = 12345;
 
         assertTrue("Palindrome did not return true.", LargestPalindromeProduct.isPalindrome(palindrome));
         assertFalse("Non-palindrome returned true.", LargestPalindromeProduct.isPalindrome(notPalindrome));
